ed2d7769a48bfdf105750b16ccc228f84132f451
YosCiiCable <YosCiiCable@gmail.com>
2023-03-04 18:00:53 +0900

    reset.shがうまくいったので

--- reset.sh
+++ reset.sh
@@ -1,12 +1,19 @@
-folderName = "temp"
+folderName="temp"
 echo -n フォルダ名: 
 read folderName
+mkdir ./archives/$folderName/
 mv ./main.go ./archives/$folderName/main.go
 
-rm ./AtCoder
+rm ./myAtCoder
 cp ./archives/main.go ./
 
+echo -n ---; echo " ~/check.sh"
+
 ~/check.sh
-#git add ./archives/*
-#git commit -m "$folderName の追加"
-#git push
+
+echo ---
+
+
+git add ./archives/*
+git commit -m "$folderName の追加"
+git push
