0924548dee8d9c7a7a461334d2e5943cc1477c71
YosCiiCable <YosCiiCable@gmail.com>
2023-03-04 19:14:25 +0900

    微調整

--- reset.sh
+++ reset.sh
@@ -1,7 +1,10 @@
-set folderName (ls | grep -v README.md | grep -v myAtCoder | grep -v archives | grep -v go.mod | grep -v main.go |grep -v reset.sh)
-mv ./main.go ./$folderName/
+echo "you have to run \"fish ./reset.sh\""
+set folderName (ls | grep -v README.md | grep -v myAtCoder | grep -v archives | grep -v go.mod | grep -v main.go | grep -v reset.sh); \
+	echo $folderName
+mv ./main.go ./$folderName/lastMain.go
 mv ./$folderName/ ./archives/
 
+cp ./archives/baseCode.go ./main.go
 rm ./myAtCoder
 
 echo -n ---; echo " ~/check.sh"
